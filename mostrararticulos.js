5669f46afc95c3cb6597861bbfc72aaf3eba68a6
facundomarquez121-ship-it <facundomarquez121@gmail.com>
2025-12-01 16:12:57 -0300

    Add files via upload

--- mostrararticulos.js
+++ mostrararticulos.js
@@ -1,6 +1,6 @@
-function mostrarArticulos(rubros){
+function mostrarArticulos(rubro){
     //guardamos el rubro elegido
     localStorage.setItem('rubroSelecionado', rubro);
     //redireccion a otra pagina,
-    window.location.href = "articulosdelrubro.html?rubro=" + rubros;
+    window.location.href = "articulosdelrubro.html";
 }
